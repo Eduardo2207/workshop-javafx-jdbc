9b97554bc8481f133fe0f24c6a5bd31787e7df29
Eduardo silva <eduardo2207@hotmail.com>
2024-04-25 20:39:49 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshopjavafx {
 	requires javafx.controls;
 	requires javafx.fxml;
+	opens  model.entities;
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
 }
